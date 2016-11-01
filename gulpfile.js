154aa7e8413978a3a355e352addcdc252172d497
Claudio Goncalves <claudiogoncalvesrp@gmail.com>
2016-11-01 16:59:51 -0200

    Intalando Browser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 //chamando os arquivos para uso no projeto
 var gulp = require("gulp");
+var browserSync = require('browser-sync').create();
 var sass = require("gulp-sass");
 var notify = require("gulp-notify");
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['thor'], function() {
+
+    browserSync.init({
+        server: {
+        	baseDir:"./"
+        },
+        browser:'chrome'
+    });
+
+    gulp.watch('./source/sass/**/*.scss',['thor']);
+	gulp.watch('./source/js/**/*.js',['aquaman']);    
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('thor', function() {
-	return gulp.src('./source/sass/**/*.scss')
-	//comprimindo css
-	.pipe(sass({outputStyle:'compressed'}))
-	//identificando momento do erro e mostrando msg de alerta
-	.on('error', notify.onError({ title: 'Erro ao compilar', message: '<%= error.message %>'}))
-	.pipe(gulp.dest('./dist/css'))
-}); 
+    return gulp.src('./source/sass/**/*.scss')
+        //comprimindo css
+		.pipe(sass({outputStyle:'compressed'}))
+        //identificando momento do erro e mostrando msg de alerta
+		.on('error', notify.onError({ title: 'Erro ao compilar', message: '<%= error.message %>'}))
+		.pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
 
 //movendo arquivos js para pasta dist
 gulp.task('aquaman', function() {
@@ -25,4 +43,4 @@ gulp.task('demolidor', function() {
 });
 
 //startando todas as tarefas so com o comando gulp
-gulp.task('default',['thor','aquaman','demolidor']);
+gulp.task('default',['serve','thor','aquaman','demolidor']);
